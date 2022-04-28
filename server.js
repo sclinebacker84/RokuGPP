c2022d527ab1c7449cdf1069cb05c43db0bd8cb7
root <root@DESKTOP-43PERFM.localdomain>
2022-04-27 21:56:33 -0400

    added webapp

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+app.use(express.static(path.join(__dirname,'static')))
+app.listen(80,() => console.log('server started'))
